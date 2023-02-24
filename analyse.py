Bug fixes, Performance boost for reconstruction error calculation

analyse.py
@@ -11,6 +11,7 @@ from scipy.optimize import linear_sum_assignment
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.model_selection import GridSearchCV
+from tqdm import tqdm
 
 from plotter import ArrayPlotter, MultiTrajectoryPlotter, TrajectoryPlotter
 from trajectory import DataTrajectory
@@ -311,19 +312,25 @@ class MultiTrajectoryAnalyser:
         for model_index, model_dict in enumerate(model_results):
             model: [ParameterModel, StreamingEstimationTransformer] = model_dict[MODEL]
             print(f'Calculating median reconstruction errors ({model.describe()})...')
+            model_dict_list = [model_dict]
+            if not from_other_traj:
+                for trajectory in self.trajectories[1:]:
+                    model_dict_list.append(trajectory.get_model_result(model_params_list[model_index], log=False))
+
             mean_list = []
-            for component in range(1, self.params[N_COMPONENTS] + 1):
+            for component in tqdm(range(1, self.params[N_COMPONENTS] + 1)):
                 score_list = []
-                for trajectory in self.trajectories:
-                    if not from_other_traj:
-                        model_dict = trajectory.get_model_result(model_params_list[model_index])
-                        model = model_dict[MODEL]
-                        input_data = trajectory.data_input(model_dict[INPUT_PARAMS])
-                        matrix_projection = model_dict[PROJECTION][0]
-                    else:
-                        input_data = trajectory.data_input(model_dict[INPUT_PARAMS])
-                        matrix_projection = model.transform(input_data)
-                    try:
+                try:
+                    for traj_index, trajectory in enumerate(self.trajectories):
+                        if not from_other_traj:
+                            model_dict = model_dict_list[traj_index]
+                            model = model_dict[MODEL]
+                            input_data = trajectory.data_input(model_dict[INPUT_PARAMS])
+                            matrix_projection = model_dict[PROJECTION][0]
+                        else:
+                            input_data = trajectory.data_input(model_dict[INPUT_PARAMS])
+                            matrix_projection = model.transform(input_data)
+
                         if isinstance(model, ParameterModel):
                             reconstructed_data = model.reconstruct(matrix_projection[:, :component], component)
                             input_data = model.convert_to_matrix(input_data)
@@ -333,13 +340,13 @@ class MultiTrajectoryAnalyser:
                                                                     mean=np.mean(input_data, axis=0))
                         score = mean_squared_error(input_data, reconstructed_data, squared=False)
                         score_list.append(score)
-                    except IndexError:
-                        warnings.warn(f"Examining eigenvector number ({component}) is missing in Model {model}")
-                        # score_list.append(0)
+                except IndexError as e:
+                    warnings.warn(str(e))
+                    break
                 mean_list.append(np.median(score_list))
             model_mean_scores[f'{str(model.describe()):35}'] = np.asarray(mean_list)
 
-        title_prefix = (f'Reconstruction Error (RE)' +
+        title_prefix = (f'Reconstruction Error (RE) ' +
                         (f'from {self.trajectories[0].filename}\n' if from_other_traj else '') +
                         f'on {self.params[N_COMPONENTS]} Principal Components ')
         ArrayPlotter(

trajectory.py
@@ -121,16 +121,17 @@ class DataTrajectory(TrajectoryFile):
         coordinates_dict = self.alpha_carbon_coordinates if ac_only else self.atom_coordinates
         return coordinates_dict[:, :, element_list]
 
-    def get_model_result(self, model_parameters: [str, dict]) -> dict:
+    def get_model_result(self, model_parameters: [str, dict], log: bool = True) -> dict:
         """
         Returns a dict of all the important result values. Used for analysing the information
         :param model_parameters:
+        :param log:
         :return: dict of the results: {MODEL, PROJECTION, EXPLAINED_VAR, INPUT_PARAMS}
         """
         if isinstance(model_parameters, str):
             model, projection = self.get_model_and_projection_by_name(model_parameters)
         else:
-            model, projection = self.get_model_and_projection(model_parameters)
+            model, projection = self.get_model_and_projection(model_parameters, log=log)
         ex_var = explained_variance(model.eigenvalues, self.params[N_COMPONENTS])
         # TODO Add explained variance to the models, and if they don't have a parameter, than calculate here
         if self.params[PLOT_TYPE] == EXPL_VAR_PLOT:
@@ -210,8 +211,10 @@ class DataTrajectory(TrajectoryFile):
         else:
             raise ValueError(f'Model with name \"{model_name}\" does not exists.')
 
-    def get_model_and_projection(self, model_parameters: dict, inp: np.ndarray = None, ):
-        print(f'Running {model_parameters}...')
+    def get_model_and_projection(self, model_parameters: dict, inp: np.ndarray = None, log: bool = True):
+        if log:
+            print(f'Running {model_parameters}...')
+
         if inp is None:
             inp = self.data_input(model_parameters)
 
@@ -277,10 +280,9 @@ class DataTrajectory(TrajectoryFile):
         if isinstance(model, ParameterModel):
             return model.reconstruct(projection[0])
         else:
-            component = self.dim[ATOMS] * self.dim[COORDINATES]
             eigenvectors = model.eigenvectors
             input_data = self.data_input(model_parameters)
-            reconstructed_matrix = reconstruct_matrix(projection[0], eigenvectors, component,
+            reconstructed_matrix = reconstruct_matrix(projection[0], eigenvectors, self.params[N_COMPONENTS],
                                                       mean=np.mean(input_data, axis=0))
             return reconstructed_matrix.reshape((self.dim[TIME_FRAMES],
                                                 self.dim[ATOMS],