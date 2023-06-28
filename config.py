3a45ff5aa57b85b61b78193bbd29532f37738603
Andrea M <23620851+andreamalhera@users.noreply.github.com>
2023-06-28 12:05:32 +0200

    Update config.py
    Updates TODO

--- config.py
+++ config.py
@@ -82,7 +82,7 @@ def get_files_and_kwargs(params: dict):
         filename_list = [f'trajectory-{i}.xtc' for i in range(1, 28 + 1)]
         kwargs = {FILENAME: filename_list[file_element], TOPOLOGY_FILENAME: 'fs-peptide.pdb',
                   FOLDER_PATH: 'data/fs-peptide'}
-    #TODO: Adjust case startswith weather to multi and merge with case 'weatherDataDK'
+    #TODO: Adjust case startswith weather to multi
     elif data_set == 'weather':
         country = trajectory_name
         folder_path = f'data/weather_data/{country}/'
